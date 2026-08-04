e22a5780073726995f7ee9f951aa6893a627fdcd
tarunbhavnani <tarun.bhavnani@gmail.com>
2026-08-04 19:14:48 +0530

    daily dashboard codes

---
+++ dashboard/main.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 21:19:45 2026
+
+@author: tarun
+flag is close > 25sma
+flag counter is consecutive days above 25sma
+"""
+
+import os
+
+os.chdir(r'C:\Users\tarun\Desktop\mech-buy\dashboard')
+
+# =============================================================================
+# get current data
+# =============================================================================
+
+from get_data import *
+
+from portfolio_manager import *
+
+from config import *
+
+TICKERS=list(set(TICKERS))
+#start_date="2026-01-01"
+
+data = download_data(TICKERS,start_date)
+
+data = prepare_indicators(data)
+
+
+# =============================================================================
+# get current portfolio
+# =============================================================================
+
+import json
+
+# Read dictionary from JSON
+with open("data\portfolio.json", "r") as f:
+    portfolio = json.load(f)
+
+# =============================================================================
+# load PM class and update prices
+# =============================================================================
+
+pm = PortfolioManager(data, portfolio, stoploss)
+
+updated_portfolio=pm.update_current_portfolio()
+
+pm.save_portfolio()
+
+
+# =============================================================================
+# get sell candidates
+# =============================================================================
+
+pm.update_current_portfolio()
+
+sell_list= pm.get_sell_list()
+
+
+# =============================================================================
+# update if sold
+# =============================================================================
+
+#sell_ticker="ZAGGLE.NS"
+
+sold = pm.sell_stock(sell_ticker)
+
+pm.save_portfolio()
+
+# =============================================================================
+# get buy candidates
+# =============================================================================
+
+buy_list = pm.get_buy_candidates(dist_low, dist_high)
+
+
+check=pd.DataFrame([data[i].iloc[-1] for i in data if i in buy_list])
+check.sort_values(by='Dist25', inplace=True)
+
+# =============================================================================
+# update if bought
+# =============================================================================
+
+#buy_ticker="ABC.NS"
+
+pm.buy_stock(buy_ticker, price=100, qty=10)
+
+pm.save_portfolio()
+
+
+# =============================================================================
+# end
+# =============================================================================
+
+
---
+++ dashboard/portfolio_manager.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 21:36:52 2026
+
+@author: tarun
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 21:19:27 2026
+
+@author: tarun
+
+
+#update highest price in such a way that it shows the highest price since purchase. if run eveyday it will get updated. if not run in some days it might miss the day highs reached
+since we are just looking at the current day high
+
+make a similar one for hour level so that can be used on fno for fno calls!
+"""
+
+import numpy as np
+import json
+
+class PortfolioManager:
+
+    def __init__(self, data, portfolio, stoploss):
+        self.data = data
+        
+        self.stoploss = stoploss
+        self.portfolio = portfolio
+
+   
+
+    # =========================================================================
+    # Update current portfolio
+    # =========================================================================
+    def update_current_portfolio(self):
+        """
+        Update current portfolio with latest prices.
+        """
+
+        for ticker in self.portfolio:
+
+            row = self.data[ticker].iloc[-1]
+
+            if row is None:
+                continue
+
+            highest_price = np.floor(row["High"])
+            
+            price = np.floor(row["Close"])
+            value= price*self.portfolio[ticker]["qty"]
+            
+
+            self.portfolio[ticker]["price"] = price
+            self.portfolio[ticker]["value"] = value
+
+            if highest_price > self.portfolio[ticker]["highest_price"]:
+                self.portfolio[ticker]["highest_price"] = price
+            
+            self.portfolio[ticker]["sl"] = np.ceil(self.portfolio[ticker]["highest_price"] * (100 - self.stoploss) / 100)
+
+        return self.portfolio
+
+    # =========================================================================
+    # Buy candidates
+    # =========================================================================
+    def get_buy_candidates(self, dist_low, dist_high):
+        """
+        Find stocks eligible for buying.
+        """
+
+        buy_list = {}
+
+        for ticker in self.data:
+
+            row = self.data[ticker].iloc[-1]
+
+            if row is None:
+                continue
+
+            if row["flag"]:
+
+                if dist_low < row["Dist25"] < dist_high:
+                    if (
+                        row["flag_counter"] > 5
+                        and row["Dist25_Change"] > 0
+                    ):
+                        buy_list[ticker] = row["Dist25"]
+
+        buy_list = dict(
+            sorted(
+                buy_list.items(),
+                key=lambda x: x[1]
+            )
+        )
+
+        return list(buy_list.keys())
+
+    # =========================================================================
+    # Sell stock
+    # =========================================================================
+    def sell_stock(self, ticker):
+
+        sold = self.portfolio.pop(ticker)
+
+        return sold
+
+    # =========================================================================
+    # Buy stock
+    # =========================================================================
+    def buy_stock(self, ticker, price, qty):
+
+        bought = {
+            "price": price,
+            "highest_price": price,
+            "qty": qty,
+            "value": price * qty,
+            "sl": np.ceil(price * (100 - self.stoploss) / 100),
+        }
+
+        self.portfolio[ticker] = bought
+
+        return self.portfolio
+    # =============================================================================
+    #     save updated portfolio
+    # =============================================================================
+    
+    def save_portfolio(self):
+        
+        with open("data\portfolio.json", "w") as f:
+            json.dump(self.portfolio, f, indent=4, default=lambda x: x.item() if isinstance(x, np.generic) else x)
+            
+    # =============================================================================
+    # get sell list    
+    # =============================================================================
+    def get_sell_list(self):
+        """
+        Find all stocks that should be sold.
+
+        Returns
+        -------
+        list
+        """
+
+        sell = []
+
+        for ticker in self.portfolio:
+            row= self.portfolio[ticker]
+
+            
+            if row["price"] <= row['sl']:
+
+                sell.append(ticker)
+
+        return sell
+    
+    
+    
+    
+    
