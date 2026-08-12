d2b7d39ab8bf84676af4290e546e00d67f39329b
tarunbhavnani <tarun.bhavnani@gmail.com>
2026-08-12 17:23:06 +0530

    new files for python classes

---
+++ backtestEngine/new1.py
@@ -0,0 +1,249 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 11 17:20:17 2026
+
+@author: tarun
+"""
+
+
+import pandas as pd
+import yfinance as yf
+import numpy as np
+from config import *
+import copy
+
+
+
+
+
+def download_data(tickers,
+                  start_date,end_date=None,
+                  auto_adjust=True):
+    """
+    Download historical OHLCV data for all tickers.
+
+    Returns
+    -------
+    dict
+        {
+            ticker : dataframe
+        }
+    """
+
+    data = {}
+
+    for ticker in tickers:
+
+        print(f"Downloading {ticker}")
+
+        try:
+
+            df = yf.download(
+                ticker,
+                start=start_date,
+                end=end_date,
+                progress=False,
+                auto_adjust=auto_adjust
+            )
+
+            if df.empty:
+                print(f"{ticker} : No Data")
+                continue
+
+            # Flatten MultiIndex if required
+            if hasattr(df.columns, "levels"):
+                df.columns = df.columns.get_level_values(0)
+
+            df = df[
+                [
+                    "Close",
+                    "High",
+                    "Low",
+                    "Open",
+                    "Volume"
+                ]
+            ].copy()
+
+            df["date"] = df.index
+            df.reset_index(drop=True, inplace=True)
+
+            df["Ticker"] = ticker
+
+            df.sort_values("date", inplace=True)
+
+            data[ticker] = df
+
+        except Exception as e:
+
+            print(f"{ticker} : {e}")
+
+    return data
+
+def download_nifty(
+                  start_date,end_date=None,
+                  auto_adjust=True):
+   
+
+   
+        try:
+
+            df = yf.download(
+                "^NSEI",
+                start=start_date, end= end_date,
+                auto_adjust=True
+            )
+
+          
+
+            # Flatten MultiIndex if required
+            if hasattr(df.columns, "levels"):
+                df.columns = df.columns.get_level_values(0)
+
+            df = df[
+                [
+                    "Close",
+                    "High",
+                    "Low",
+                    "Open",
+                    "Volume"
+                ]
+            ].copy()
+
+            df["date"] = df.index
+            df.reset_index(drop=True, inplace=True)
+
+            df["Ticker"] = 'nifty'
+
+            df.sort_values("date", inplace=True)
+
+      
+        except Exception as e:
+
+            print(f"{e}")
+
+        return df
+
+
+
+
+def flag_counter(df):
+    counter=0
+    flag_counter=[]
+    for i in df['flag']:
+        if i:
+            counter+=1
+            flag_counter.append(counter)
+        else:
+            counter=0
+            flag_counter.append(counter)
+    df['flag_counter'] =flag_counter
+    return df
+
+def anti_flag_counter(df):
+    counter=0
+    anti_flag_counter=[]
+    for i in df['flag']:
+        if i:
+            counter=0
+            anti_flag_counter.append(counter)
+        else:
+            counter+=1
+            anti_flag_counter.append(counter)
+    df['anti_flag_counter'] =anti_flag_counter
+    return df
+
+
+def get_day(data):
+    max_len=max([len(data[i]) for i in data])
+    temp_stock= [i for i in data if len(data[i])==max_len][0]
+    temp_data= copy.deepcopy(data[temp_stock])
+    temp_data["day"]= range(1, len(temp_data) + 1)
+    temp_data=temp_data[['date', 'day']]
+    
+    for i in data:
+        data[i]= data[i].merge(temp_data, on='date', how='left')
+    
+    return data
+
+def prepare_indicators(data):
+    """
+    Adds all indicators to every dataframe.
+
+    Parameters
+    ----------
+    data : dict
+
+    Returns
+    -------
+    dict
+    """
+
+    for ticker in data:
+
+        df = data[ticker]
+
+        # Moving averages
+
+        df["SMA25"] = df["Close"].rolling(25).mean()
+        df["SMA100"] = df["Close"].rolling(100).mean()
+        df["SMA200"] = df["Close"].rolling(200).mean()
+
+        # Volume average
+
+        df["VOL25"] = df["Volume"].rolling(25).mean()
+
+        # Distance from moving averages
+
+        df["Dist25"] = (
+            (df["Close"] - df["SMA25"])
+            / df["SMA25"]
+            * 100
+        )
+
+        df["Dist100"] = (
+            (df["Close"] - df["SMA100"])
+            / df["SMA100"]
+            * 100
+        )
+
+        df["Dist200"] = (
+            (df["Close"] - df["SMA200"])
+            / df["SMA200"]
+            * 100
+        )
+        
+        #rising or falling
+        df["Dist25_Change"] = df["Dist25"].diff()
+        
+        
+        # buy signal
+
+        df["flag"] = df["Close"] > df["SMA25"]
+        
+        df=flag_counter(df)
+        df=anti_flag_counter(df)
+        
+
+        data[ticker] = df
+
+    return data
+
+
+
+
+def get_final_data(all_data,nifty, TICKERS):
+    data= {i:j for i,j in all_data.items() if i in TICKERS}    
+    
+    
+    data = prepare_indicators(data)
+    
+    data=get_day(data)
+    
+    nifty = prepare_indicators({'nifty':nifty})
+    
+    nifty=get_day(nifty)
+    
+    
+    return data, nifty['nifty']
+
+
---
+++ backtestEngine/new2.py
@@ -0,0 +1,614 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 11 18:50:09 2026
+
+@author: tarun
+DAY N
+ │
+ ├── Portfolio empty?
+ │      │
+ │      └── YES
+ │           │
+ │           └── Find buy candidates
+ │                 │
+ │                 ├── Nifty anti_flag_counter <= 5
+ │                 ├── flag = True
+ │                 ├── Dist25 within range
+ │                 ├── flag_counter > 5
+ │                 └── Angle > 10
+ │
+ │           Sort by Dist25 DESC
+ │           Buy up to MAX_POSITIONS
+ │
+ │
+ └── Portfolio not empty
+        │
+        ├── Mark to market
+        │
+        ├── Update trailing SL
+        │
+        ├── Find sells
+        │      │
+        │      └── Close < SMA25 × 0.98
+        │          AND anti_flag_counter > 5
+        │          AND Angle < 0
+        │
+        ├── Sell at Close
+        │
+        ├── Positions < MAX_POSITIONS?
+        │      │
+        │      └── YES
+        │           │
+        │           └── Find new buy candidates
+        │               Buy available slots
+        │
+        └── Mark to market again
+"""
+
+
+import pandas as pd
+import yfinance as yf
+import numpy as np
+from config import *
+import copy
+import random
+
+
+
+import numpy as np
+import json
+
+class PortfolioManager:
+
+    def __init__(self, data, nifty,stoploss, first_day, money, max_positions,dist_low,dist_high):
+        self.data = data
+        self.first_day=first_day
+        self.stoploss = stoploss
+        self.portfolio = {}
+        self.owned=[]
+        self.money=money
+        self.max_positions=max_positions
+        self.dist_low=dist_low
+        self.dist_high=dist_high
+        self.nifty=nifty
+
+   
+
+
+
+
+
+
+    def get_row(self, df, day):
+    
+        row = df.loc[df["day"] == day]
+    
+        if row.empty:
+            return None
+    
+        return row.iloc[0]
+
+
+
+
+
+    def build_initial_portfolio(self,day):
+        """
+        Build initial portfolio.
+    
+        Returns
+        -------
+        portfolio
+        cash
+        owned
+        """
+    
+        
+    
+        #money = capital
+        buy_list = self.get_buy_candidates(day)
+        buy_list = buy_list[:self.max_positions]
+        
+        allocation_per_stock=np.floor(self.money/self.max_positions)
+        
+        
+        bought=[]
+    
+        for ticker in buy_list:
+    
+            row = self.get_row(self.data[ticker], day)
+    
+            if row is None:
+                continue
+    
+            price = np.floor(row["Close"])
+            
+            sl=np.ceil(price*(100-self.stoploss)/100)
+    
+            qty = int(allocation_per_stock // price)
+    
+            cost = qty * price
+    
+            if cost > self.money:
+                continue
+    
+            self.portfolio[ticker] = {
+    
+                "price": price,
+    
+                "qty": qty,
+    
+                "value": cost,
+                "sl":sl
+    
+            }
+    
+            self.money -= cost
+            
+            bought.append((ticker, price, qty))
+    
+        self.owned = list(self.portfolio.keys())
+    
+        return  bought
+
+
+    def get_sell_list(self,day):
+        """
+        Find all stocks that should be sold.
+    
+        Returns
+        -------
+        list
+        """
+    
+        sell = []
+    
+        for ticker in self.portfolio:
+    
+            row = self.get_row(self.data[ticker], day)
+    
+            if row is None:
+                continue
+    
+            #if row["Close"] < portfolio[ticker]['sl']:
+            #if row["Close"] < row['SMA25']*.98 and row['anti_flag_counter']>9:
+            if row["Close"] < row['SMA25']*.98 and row['anti_flag_counter']>5 and row['Angle']<0:
+            
+    
+                sell.append(ticker)
+    
+        return list(set(sell))
+
+
+    def execute_sells(self,
+                      sell_list,
+                      
+                      day
+                      ):
+        """
+        Execute all sells.
+    
+        Returns
+        -------
+        portfolio
+        money
+        owned
+        """
+        sold=[]
+        for ticker in sell_list:
+    
+            row = self.get_row(self.data[ticker], day)
+    
+            if row is None:
+                continue
+    
+            sell_price = row["Close"]
+    
+            qty = self.portfolio[ticker]["qty"]
+    
+            self.money += sell_price * qty
+            
+            sold.append((ticker, sell_price, qty))
+    
+            temp=self.portfolio.pop(ticker)
+            #print("sell:", temp)
+            
+    
+        self.owned = list(self.portfolio.keys())
+    
+    
+        return sold
+
+
+    def sell_all(self,day):
+        """
+        Execute all sells.
+    
+        Returns
+        -------
+        portfolio
+        money
+        owned
+        """
+        sold=[]
+        
+        for ticker in self.portfolio:
+    
+            row = self.get_row(self.data[ticker], day)
+    
+            if row is None:
+                continue
+    
+            sell_price = row["Close"]
+    
+            qty = self.portfolio[ticker]["qty"]
+    
+            self.money += sell_price * qty
+            
+            sold.append((ticker, sell_price, qty))
+    
+            #temp=portfolio.pop(ticker)
+            #print("sell:", temp)
+            
+        return  sold
+
+    def get_buy_candidates(self,day):
+                           
+        """
+        Find stocks eligible for buying.
+    
+        Returns
+        -------
+        list
+        """
+        try:
+            nifty_angle=self.get_nifty_angle(day)
+        except:
+            nifty_angle=0
+        
+        if nifty_angle>5:
+            return []
+    
+        buy_list = {}
+    
+        for ticker in self.data:
+    
+            if ticker in self.owned:
+                continue
+    
+            row = self.get_row(self.data[ticker], day)
+    
+            if row is None:
+                continue
+    
+            if row["flag"]:
+    
+                if self.dist_low < row["Dist25"] < self.dist_high:
+               
+                    if row['flag_counter']>5 and row["Angle"]>10 :
+               
+                        buy_list[ticker] = row["Dist25"]
+    
+        buy_list = dict(
+            sorted(
+                buy_list.items(),
+                key=lambda x: x[1],reverse=True
+            )
+        )
+    
+        return list(buy_list.keys())
+
+    def execute_buys(self,buy_list,day):
+        """
+        Buy new stocks.
+    
+        Returns
+        -------
+        portfolio
+        money
+        owned
+        """
+    
+        #owned = list(portfolio.keys())
+    
+        available = self.max_positions - len(self.owned)
+        
+        
+    
+        if available <= 0:
+            return 
+        
+        allocation_per_stock=np.floor(self.money/available)
+        
+        buy_list = buy_list[:available]
+        bought=[]
+    
+        for ticker in buy_list:
+    
+            row = self.get_row(self.data[ticker], day)
+    
+            if row is None:
+                continue
+    
+            price = row["Close"]
+            
+            if self.money>allocation_per_stock:
+    
+                qty = int(allocation_per_stock // price)
+            else:
+                qty = int(self.money // price)
+            
+    
+            if qty <= 0:
+                continue
+    
+            cost = qty * price
+    
+            if cost > self.money:
+                continue
+            
+            sl=np.ceil(price*(100-self.stoploss)/100)
+    
+            self.portfolio[ticker] = {
+    
+                "price": price,
+    
+                "qty": qty,
+    
+                "value": cost,
+                
+                "sl":sl
+    
+            }
+            #print("Buy:",portfolio[ticker])
+    
+            self.money -= cost
+            
+            bought.append((ticker, price, qty))
+    
+        
+    
+        return bought
+
+
+    def get_nifty_angle(self,day):
+        row = self.get_row(self.nifty, day)
+        #return row['Angle']
+        return row['anti_flag_counter']
+    
+        
+
+
+
+    def mark_to_market(self,day):
+        """
+        Update portfolio prices to current day.
+    
+        Returns
+        -------
+        portfolio
+        """
+    
+        for ticker in self.portfolio:
+            
+            row = self.get_row(self.data[ticker], day)
+    
+            if row is None:
+                continue
+    
+            self.portfolio[ticker]["price"] = row["Close"]
+            self.portfolio[ticker]["value"] = (
+                row["Close"] *
+                self.portfolio[ticker]["qty"]
+            )
+            
+            new_sl=np.floor(row["High"]*(100-self.stoploss)/100)
+            
+            if new_sl>self.portfolio[ticker]["sl"]:
+                self.portfolio[ticker]["sl"]=new_sl
+    
+        
+
+
+    def calculate_portfolio_value(self):
+        """
+        Calculate portfolio value.
+    
+        Returns
+        -------
+        portfolio_value
+        total_equity
+        """
+    
+        portfolio_value = sum(
+            position["value"]
+            for position in self.portfolio.values()
+        )
+    
+        total_equity = portfolio_value + self.money
+    
+        return portfolio_value, total_equity
+
+
+
+
+    def run_backtest(self):
+    
+        max_day = max(df["day"].max() for df in self.data.values())
+        
+        
+        all_bought={}
+        all_sold={}
+    
+    
+        history = []
+    
+        for day in range(self.first_day, max_day + 1):
+            
+            print(day, end=", ", flush=True)
+            
+            
+            
+            if len(self.owned)==0 :
+                
+                
+                
+                bought = self.build_initial_portfolio(day)
+                all_bought[day]=bought
+            
+                
+            
+            else:
+    
+                
+                #get marhet value
+                self.mark_to_market(day)
+                
+                #get sell candidates
+                sell_list = self.get_sell_list(day)
+                
+                #sell
+                sold = self.execute_sells(sell_list, day)
+                all_sold[day]=sold
+                
+                
+                #buy new
+                if len(self.owned) < self.max_positions :
+                    
+                    #get buy list
+                    buy_list = self.get_buy_candidates(day)
+                    
+                    
+                    #buy
+                    bought = self.execute_buys( buy_list, day)
+                    all_bought[day]=bought
+        
+                
+                #get marhet value
+                self.mark_to_market(day)
+                    
+            #get portfolio value
+            portfolio_value, equity = self.calculate_portfolio_value()
+                    
+                #get date
+                #sample = get_row(next(iter(data.values())),day)
+                
+            current=[i+'-'+str(int(self.portfolio[i]['price']))+'-'+str(int(self.portfolio[i]['sl'])) for i in self.portfolio]
+    
+            history.append({
+                "day": day,
+            #    "date": sample["date"],
+                "cash": self.money,
+                "portfolio": portfolio_value,
+                "equity": equity,
+                "positions": len(self.portfolio),
+                "current":copy.deepcopy(current)
+                
+            })
+    
+        history = pd.DataFrame(history)
+        
+        history=self.get_date(history)
+    
+        return history, all_bought, all_sold
+
+
+    def get_date(self,history):
+    
+        date_len=max([len(self.data[i]) for i in self.data])
+        
+        ticker=[i for i in self.data if len(self.data[i])==date_len][0]
+        
+        temp=self.data[ticker][['date', 'day']]
+        
+        history= history.merge(temp, on='day', how='left')
+        
+        return history
+
+
+
+
+def performance_metrics(history,
+                        initial_capital):
+
+    history = history.copy()
+
+    history["Peak"] = history["equity"].cummax()
+
+    history["Drawdown"] = (
+        history["equity"]
+        - history["Peak"]
+    ) / history["Peak"]
+
+    max_drawdown = history["Drawdown"].min()
+
+    years = (
+        history["date"].iloc[-1]
+        - history["date"].iloc[0]
+    ).days / 365.25
+
+    ending_value = history["equity"].iloc[-1]
+
+    cagr = (
+        (ending_value / initial_capital)
+        ** (1 / years)
+        - 1
+    )
+
+    print("=" * 50)
+    print(f"Initial Capital : {initial_capital:,.0f}")
+    print(f"Ending Equity   : {ending_value:,.0f}")
+    print(f"CAGR            : {cagr:.2%}")
+    print(f"Max Drawdown    : {max_drawdown:.2%}")
+    print("=" * 50)
+
+    return {
+        "Ending Equity": ending_value,
+        "CAGR": cagr,
+        "Max Drawdown": max_drawdown
+    }
+
+
+import matplotlib.pyplot as plt
+
+def plot_equity_curve(history):
+
+    plt.figure(figsize=(12,6))
+
+    plt.plot(
+        history["date"],
+        history["equity"],
+        linewidth=2
+    )
+
+    plt.title("Portfolio Equity Curve")
+
+    plt.xlabel("Date")
+
+    plt.ylabel("Portfolio Value")
+
+    plt.grid(True)
+
+    plt.show()
+    
+    
+
+# =============================================================================
+# 
+# =============================================================================
+
+def get_transactions(all_bought, all_sold):
+    final=[]
+    for i in all_bought:
+        for j in all_bought[i]:
+            final.append((i,'buy', j[0],j[1],j[2]))
+    
+    
+    for i in all_sold:
+        for j in all_sold[i]:
+            final.append((i,'sell', j[0],j[1],j[2]))
+    
+    
+    final=pd.DataFrame(final)
+    final.columns=['day','action', 'ticker', 'price', 'qty']
+    return final
